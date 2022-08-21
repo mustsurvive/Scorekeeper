6d692a837af50c843d3970021bb0516b23ec151b
mustsurvive <110630103+mustsurvive@users.noreply.github.com>
2022-08-21 23:24:53 +0200

    Scorekeeper

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+function bot1(){
+  let a= document.getElementById("brek").innerHTML;
+  let b= Number(a)+ Number(1);
+  document.getElementById("brek").innerHTML=b;
+}
+
+
+
+
+
+
+
+function bot2(){
+  let c= document.getElementById("brek").innerHTML;
+  let b= Number(c) + Number(2);
+  document.getElementById("brek").innerHTML=b;
+}
+// function bot2() { 
+//   brek.style.color="#97f5bb";
+//   let artik = document.getElementById("brek").innerHTML; 
+//   let artik2 = Number(artik) + Number(2);
+//   document.getElementById("brek").innerHTML=artik2;}
+function bot3(){
+  let tisch= document.getElementById("brek").innerHTML;
+  let stuhl= Number(tisch)+ Number(3);
+  document.getElementById("brek").innerHTML=stuhl;
+}
+
+function botto1(){
+  let hose=document.getElementById("fixo").innerHTML
+  let gürtel= Number(hose) + Number(1)
+  document.getElementById("fixo").innerHTML=gürtel;
+}
+
+
+function botto2(){
+  let fluss=document.getElementById("fixo").innerHTML
+  let rhein=Number(fluss)+Number(2);
+  fluss=document.getElementById("fixo").innerHTML=rhein
+}
+
+
+function botto3(){
+  let basketball=document.getElementById("fixo").innerHTML;
+  let basket= Number(basketball) + Number(3);
+  basketball=document.getElementById("fixo").innerHTML=basket
+}
+
+
+function spon() {
+  document.getElementById("brek").innerHTML= "0";
+  document.getElementById("fixo").innerHTML= "0";
+}
+
+
